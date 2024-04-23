95498c0ee6d232bc186bc3935413a63988a6b82c
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-23 10:26:39 +0200

    Spring boot project created

---
+++ spabackend/src/main/java/com/spabackend/spabackend/SpabackendApplication.java
@@ -0,0 +1,13 @@
+package com.spabackend.spabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpabackendApplication.class, args);
+	}
+
+}
---
+++ spabackend/src/test/java/com/spabackend/spabackend/SpabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spabackend.spabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
