6985b70d377406eec98339d608d0d38dc20ed523
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-24 11:53:16 +0200

    Lagt till för att tillåta origins

--- spabackend/src/main/java/com/spabackend/spabackend/SpabackendApplication.java
+++ spabackend/src/main/java/com/spabackend/spabackend/SpabackendApplication.java
@@ -2,12 +2,28 @@ package com.spabackend.spabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpabackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpabackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*") // Tillåt alla origins
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
